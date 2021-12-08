70ff8cab62ada6c3c32664d63334232b0aa391a5
PatriciaVel <marta.velandia.mt@correo.usa.edu.co>
2021-12-08 11:14:27 -0500

    ajuste main

--- src/main/java/Ciclo4/Ciclo4Application.java
+++ src/main/java/Ciclo4/Ciclo4Application.java
@@ -1,13 +1,28 @@
 package Ciclo4;
 
+import Ciclo4.interfaces.InterfaceUser;
+import Ciclo4.interfaces.InterfaceVegetarian;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ciclo4Application {
+public class Ciclo4Application implements CommandLineRunner{
+    @Autowired
+    private InterfaceVegetarian interfaceVegetarian;
+    @Autowired
+    private InterfaceUser interfaceUser;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ciclo4Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Ciclo4Application.class, args);
+    }
+    
+    @Override
+    public void run(String... args) throws Exception{
+        interfaceVegetarian.deleteAll();
+        interfaceUser.deleteAll();
+    }
+        
 
 }
